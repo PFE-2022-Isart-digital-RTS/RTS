7633a4921c47cff37c7553b272695a32caf5c51c
Baptiste SUZON <b.suzon@student.isartdigital.com>
2022-04-13 16:58:17 +0200

    Lobby refacto

--- Assets/Scripts/Client/Lobby.cs
+++ Assets/Scripts/Client/Lobby.cs
@@ -1,35 +1,42 @@
 using Unity.Netcode;
+using Unity.Netcode.Transports.UNET;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Lobby : MonoBehaviour
 {
+    public Button loadSceneButton;
     public Button hostButton;
     public Button clientButton;
     public Button serverButton;
-    
+
+    public InputField IpInputField;
+
     // Start is called before the first frame update
     void Start()
     {
+        loadSceneButton.enabled = false;
+        loadSceneButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+            NetworkManager.Singleton.SceneManager.OnLoadComplete += LoadMap;
+        });
         hostButton.onClick.AddListener(() =>
         {
             Debug.Log("StartHost success");
-            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("SampleScene");
-            asyncLoad.completed += operation =>
+
+            if (NetworkManager.Singleton.StartHost())
             {
-                if (NetworkManager.Singleton.StartHost())
-                {
-                    SceneManager.LoadScene("Map", LoadSceneMode.Additive);
-                }
-                else
-                {
-                    SceneManager.LoadScene("Menu");
-                    Debug.Log("StartHost fail");
-                }
-            };
+                loadSceneButton.enabled = true;
+            }
+            else
+            {
+                SceneManager.LoadScene("Menu");
+                Debug.Log("StartHost fail");
+            }
         });
-        
+
         clientButton.onClick.AddListener(() =>
         {
             if (NetworkManager.Singleton.StartClient())
@@ -41,7 +48,7 @@ public class Lobby : MonoBehaviour
                 Debug.Log("StartClient fail");
             }
         });
-        
+
         serverButton.onClick.AddListener(() =>
         {
             if (NetworkManager.Singleton.StartServer())
@@ -53,5 +60,17 @@ public class Lobby : MonoBehaviour
                 Debug.Log("serverButton fail");
             }
         });
+
+        IpInputField.text = NetworkManager.Singleton.GetComponent<UNetTransport>().ConnectAddress;
+        IpInputField.onValueChanged.AddListener(ipAdress =>
+        {
+            NetworkManager.Singleton.GetComponent<UNetTransport>().ConnectAddress = ipAdress;
+        });
+    }
+    
+    void LoadMap(ulong id,string sceneName, LoadSceneMode mode)
+    {
+        NetworkManager.Singleton.SceneManager.LoadScene("Map", LoadSceneMode.Additive);
+        NetworkManager.Singleton.SceneManager.OnLoadComplete -= LoadMap;
     }
 }
